51c3a6d9f240a66878ba12e57400353c22a483ad
EclypDev <hectoraderfer123421@gmail.com>
2023-07-05 12:50:52 +0800

    Uodate Method Date.js

---
+++ Date.js
@@ -0,0 +1,6 @@
+const año = new Date().getFullYear();
+const dia = new Date().getDay();
+const hora = new Date().getHours();
+const minute = new Date().getMinutes();
+
+console.log(`${año}/${dia}/${hora}/${minute}`);
